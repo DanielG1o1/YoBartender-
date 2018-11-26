abb913063cb933f48f1515fbfe50cbcdfd0fa6b7
Aaron Meighoo <aaronmeighoo1@gmail.com>
2018-11-25 21:33:36 -0400

    Made minor changes to 'recordFaces' to suit the coding standard better. Changed the name of 'detector' to 'recognizer' since it's a more accurate name

--- recordFaces_Ver1.3_AM.py
+++ recordFaces_Ver1.3_AM.py
@@ -11,7 +11,7 @@ conn = sqlite3.connect('database2.db')
 if not os.path.exists('./dataset'):
     os.makedirs('./dataset')
 
-# This variable is the ID generator. It generates a unique 4-digit Identifier    for each person from 1000-9999
+# This variable is the ID generator. It generates a unique 4-digit Identifier  for each person from 1000-9999
 four_dig_id = 1000
 # Variable checks if another user wants to enter the system. It is used by LOOP A which controls the writing of information to the database and images to the 'dataset' folder
 NEXT_USER = "Y"  
@@ -19,42 +19,39 @@ NEXT_USER = "Y"
 # This loop (LOOP A) will ensure that persons can be entered successively
 while (NEXT_USER == 'Y'):  # Terminating condition
   
-  four_dig_id = four_dig_id + 1  # Incrementing the ID number for assignment                                    to another person
-  c = conn.cursor()  # Inititalizing a cursor for information storage
+    four_dig_id = four_dig_id + 1  # Incrementing the ID number for assignment                                    to another person
+    c = conn.cursor()  # Inititalizing a cursor for information storage
 
-  # 'CascadeClassifier' allows specification for the cascade to be used, and will be read from a file which, in this case, is 'haarcascadeFrontalfaceDefault_Ver1.0.xml'
-  face_cascade = cv2.CascadeClassifier('haarcascadeFrontalfaceDefault_Ver1.0.xml')
-  cap = cv2.VideoCapture(0)
-  uname = input("Enter your name: ")  # User will enter their name
-  udrink = input("Enter your drink preference: ")  # User will enter their                                                      drink preference for the                                                     night
+    # 'CascadeClassifier' allows specification for the cascade to be used, and will be read from a file which, in this case, is 'haarcascadeFrontalfaceDefault_Ver1.0.xml'
+    face_cascade = cv2.CascadeClassifier('haarcascadeFrontalfaceDefault_Ver1.0.xml')
+    cap = cv2.VideoCapture(0)
+    uname = input("Enter your name: ")  # User will enter their name
+    udrink = input("Enter your drink preference: ")  # User will enter their                                                      drink preference for                                                      the night
 
-  #This inserts all the data that the users entered, and the autmaticall generated ID number into the database
-  c.execute('INSERT INTO users (Name, Drink_Preference, Four_Digit_Identifier) VALUES (?,?,?)', (uname,udrink,four_dig_id))
-  uid = c.lastrowid  # Specifies that each new entry should appear at the last                    empty row in the database
+    #This inserts all the data that the users entered, and the autmaticall generated ID number into the database
+    c.execute('INSERT INTO users (Name, Drink_Preference, Four_Digit_Identifier) VALUES (?,?,?)', (uname,udrink,four_dig_id))
+    uid = c.lastrowid  # Specifies that each new entry should appear at the                         last empty row in the database
   
-  sampleNum = 0  # This variable controls the amount of images taken to be                    stored in the 'dataset' folder
-  while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    sampleNum = 0  # This variable controls the amount of images taken to be                    stored in the 'dataset' folder
+    while True:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    for (x,y,w,h) in faces:
-      sampleNum = sampleNum+1
-      cv2.imwrite("dataset/" + uname + "." +str(uid)+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
-      cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-      cv2.waitKey(100)
+        for (x,y,w,h) in faces:
+            sampleNum = sampleNum+1
+            cv2.imwrite("dataset/" + uname + "." +str(uid)+"."+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
+            cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+            cv2.waitKey(100)
 
-    cv2.imshow('img',img)
-    cv2.waitKey(1);
-    if sampleNum > 20:
-      break
+        cv2.imshow('img',img)
+        cv2.waitKey(1);
+        if sampleNum > 20:
+            break
 
-  print("Enter new user? (Press 'Y' for YES & 'N' for NO")
-  NEXT_USER = input("---->")  # Terminating condition is when NEXT_USER == 'N'
+    print("Enter new user? (Press 'Y' for YES & 'N' for NO")
+    NEXT_USER = input("---->")  # Terminating condition is when NEXT_USER=='N'
   
-#End all data capture
-
-
 cap.release()
 conn.commit()  # Commits changes to the database
 conn.close()  # Closes connection to the database
